5db8c392d897bab4bac2473f73b06556020a8b3b
Moritz Beller <moritzbeller@gmx.de>
2017-01-13 09:59:01 +0100

    Add kill long running processes script

--- general/general.sh
+++ general/general.sh
@@ -1,1 +1,5 @@
-time some_command 2>&1 | tee out.log # time and execute command some_command, redirecting all output (error stream and standard output) to out.log and mirror current contents of out.log on the console
+# time and execute command some_command, redirecting all output (error stream and standard output) to out.log and mirror current contents of out.log on the console
+time some_command 2>&1 | tee out.log 
+
+# sum number of lines of files in (sub-)directories
+find . -iname "*.log" | xargs wc -l | awk '{print $1}' | paste -sd+ - | bc
---
+++ kill-long-running-processes/kill-long-processes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Source: http://serverfault.com/questions/166034/kill-all-processes-that-are-running-for-more-than-5-minutes-by-a-given-user-in-l
+
+# This script will kill process which running more than X hours
+# egrep: the selected process; grep: hours
+PIDS="`ps eaxo bsdtime,pid,comm | egrep "spamd|exim|mysqld|httpd" | grep " 1:" | awk '{print $2}'`"
+
+# Kill the process
+echo "Killing spamd, exim, mysqld and httpd processes running more than one hour..."
+for i in ${PIDS}; do { echo "Killing $i"; kill -9 $i; }; done;
